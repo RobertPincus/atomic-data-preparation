48109e25925cd14211c1b2756d86179c58124109
Robert Pincus <Robert.Pincus@colorado.edu>
2021-05-05 15:48:45 -0400

    Flight level v1.1: changed variables names for IR temperatures and SFMR quantities; added TIR_up

--- extract_flight_level_summary.py
+++ extract_flight_level_summary.py
@@ -21,9 +21,21 @@ campaign = "EUREC4A"
 project = "ATOMIC"
 platform = "P3"
 product = "Flight-Level"
-data_version = "v1.0"
+data_version = "v1.1"
 filePrefix = "{}_{}_{}_{}".format(campaign, project, platform, product)
 dataDir = pathlib.Path("data/flight-level-summary")
+
+flight_dates = [datetime.date(2020, 1, 17),
+                datetime.date(2020, 1, 19),
+                datetime.date(2020, 1, 23),
+                datetime.date(2020, 1, 24),
+                datetime.date(2020, 1, 31),
+                datetime.date(2020, 2,  3),
+                datetime.date(2020, 2,  4),
+                datetime.date(2020, 2,  5),
+                datetime.date(2020, 2,  9),
+                datetime.date(2020, 2, 10),
+                datetime.date(2020, 2, 11)]
 
 #
 # Mapping between variables in the summary and the file provided by AOC
@@ -37,9 +49,8 @@ var_mapping = {
     "cog":"TRK.d",
     "ws":"WS.d",
     "wd":"WD.d",
-    "sfmrU":"SfmrWS.1",
-    "sfmrR":"SfmrRainRate.1",
-#    "sst":"TRadD.1",
+    "U10_sfmr":"SfmrWS.1",
+    "RainRate_sfmr":"SfmrRainRate.1",
     "gs":"GS.d",
     "hed":"THDGref",
     "tas":"TAS.d", #
@@ -47,8 +58,9 @@ var_mapping = {
     "Td":"TD.c",
     "Ta":"TA.d",
     "RH":"HUM_REL.d",
-    "TradD":"TRadD.1",
-    "TradS":"TRadS.1",
+    "TIR_down":"TRadD.1",
+    "TIR_side":"TRadS.1",
+    "TIR_up"  :"TRadU.1",
     "cab":"PCAB.1",
     "wvel":"UWZ.d",
     "uvel":"UWX.d",
@@ -68,30 +80,38 @@ name_mapping = {
     "Ta":"air_temperature",
     "RH":"relative_humidity"}
 
-for input_file in sorted(dataDir.joinpath("Level_1").glob("2020*_A*.nc")):
-    aoc_file_name = input_file.name
-    print ("Opening " + aoc_file_name)
-    full = xr.open_dataset(input_file, decode_times = False)
+#
+# Would have been great to do this over OpenDAP but some variables (e.g. TRK.d) can't be read??
+#  Even better to have gotten the names of the file automatically but that's fussy
+#
+
+for d in flight_dates:
+    aoc_file_name = dataDir.joinpath("Level_1").joinpath(f"{d:%Y%m%d}" + "I1_")
+    print (aoc_file_name)
+    try:
+        full = xr.open_dataset(aoc_file_name.as_posix() + "AC.nc",  decode_times = False)
+    except:
+        full = xr.open_dataset(aoc_file_name.as_posix() + "AXC.nc", decode_times = False)
+
     #
     # Construct a time index
     #
     # Mask is false where there are NAs
     timeMask =xr.ufuncs.logical_not(np.logical_or(np.logical_or(xr.ufuncs.isnan(full.HH),
-                                                                xr.ufuncs.isnan(full.MM)), xr.ufuncs.isnan(full.SS)))
+                                                                xr.ufuncs.isnan(full.MM)),
+                                                                xr.ufuncs.isnan(full.SS)))
     hours = full.HH.where(timeMask, drop=True)
     mins  = full.MM.where(timeMask, drop=True)
     secs  = full.SS.where(timeMask, drop=True)
-    # Don't see year/month/day in the file data or metadata so we'll decode from file name...
-    year  = int(aoc_file_name[0:4])
-    month = int(aoc_file_name[4:6])
-    day   = int(aoc_file_name[6:8])
     #   Hours, mins, secs are in UTC time
     #   File names are consistent with this - night flight were 8/9, 9/10, 10/11 local time
     #   I ensured that all flights begin and end on the same UTC day, and all night flights were in the same month
     #
     # Create a new dataset with time coordinates
     #
-    subset = xr.Dataset(coords = {'time':[datetime.datetime(year, month, day, hours[i], mins[i], secs[i]) for i in range(hours.size)]})
+    subset = xr.Dataset(coords = {'time':[datetime.datetime(d.year, d.month, d.day,
+                                                            hours[i], mins[i], secs[i])
+                                                            for i in range(hours.size)]})
     #
     # Should we also remove time on the ground?
     #
@@ -103,13 +123,6 @@ for input_file in sorted(dataDir.joinpath("Level_1").glob("2020*_A*.nc")):
                                    dims={"time"},
                                    coords={"time":subset.time},
                                    attrs = atts)
-    #
-    # Water vapor mixing ratio - Chris originally included this derived parameter but I'm not going to,
-    #   leaving it instead for users to compute themselves
-    #
-    if False:
-        subset["q"] = qair3(subset.press,  subset.Ta, subset.RH)
-        subset.q.attrs = {"units":"g/kg", "Description":"Water vapor mixing ratio (computed from press, Ta, RH)", "standard_name":"humidity_mixing_ratio"}
 
     #
     # CF compliance
@@ -138,8 +151,7 @@ for input_file in sorted(dataDir.joinpath("Level_1").glob("2020*_A*.nc")):
 
     L2_dir = dataDir.joinpath("Level_2")
     L2_dir.mkdir(parents=True, exist_ok=True)
-    fileName  = filePrefix + "_{:04d}{:02d}{:02d}".format(year,month,day)
-    # fileName += "_{:02d}{:02d}{:02d}".format(int(hours[0].values), int(mins[0].values), int(secs[0].values)) + ".nc"
+    fileName  = filePrefix + f"{d:%Y%m%d}"
     fileName += "_" + data_version + ".nc"
     print("Writing " + fileName)
     subset.attrs = {"creation_date":time.strftime("%Y-%m-%d %H:%M:%S UTC", time.gmtime()),
@@ -150,23 +162,7 @@ for input_file in sorted(dataDir.joinpath("Level_1").glob("2020*_A*.nc")):
                     "product":product,
                     "contact":"Chris Fairall <Chris.Fairall@noaa.gov>",
                     "version":data_version}
-    subset.to_netcdf(L2_dir.joinpath(fileName), encoding={"time":{"units":"seconds since 2020-01-01","dtype":"double"}})
+    subset.to_netcdf(L2_dir.joinpath(fileName),
+                     encoding={"time":{"units":"seconds since 2020-01-01","dtype":"double"}})
     subset.close()
     full.close()
-
-
-#
-# Would have been great to do this over OpenDAP
-#  Even better to have gotten the names of the file automatically but that's fussy
-#
-#flight_dates = ["0117", "0119", "0123", "0124", "0131", "0203", "0204", "0205", "0209", "0210", "0211"]
-#server_root = "http://psl.noaa.gov/thredds/dodsC/Datasets/ATOMIC/data/p3/flight_level/Level_1/"
-#for fd in flight_dates:
-#    aoc_file_name = "2020" + fd + "I1_"
-#    if fd is "0131" or fd is "0211":
-#        aoc_file_name += "AXC.nc"
-#    else:
-#        aoc_file_name += "AC.nc"
-#    print ("Opening " + aoc_file_name)
-
-#    full = xr.open_dataset(server_root + aoc_file_name, decode_times = False)
