c6713bc22c99bc2a84f099001ba83c254189d906
Robert Pincus <Robert.Pincus@colorado.edu>
2021-05-10 22:48:30 -0400

    Unique long names for each variable

--- combine_microphysics.py
+++ combine_microphysics.py
@@ -39,6 +39,11 @@ for d in dates:
     synth = xr.merge([xr.open_dataset(dataDir.joinpath(l + suffix)).rename({
                         v:l + "_" + v for v in ["number_concentration", "effective_radius"]})
                         for l in ["hydrometeor", "aerosol"]])
+    for l in ["hydrometeor", "aerosol"]:
+        synth[l + "_number_concentration"].attrs["long_name"] = "number_concentration_" + l
+        synth[l + "_effective_radius"].attrs    ["long_name"] = "effective_radius_"     + l
+        synth[l + "_size_bnds"].attrs           ["long_name"] = "size_bin_boundaries_"  + l
+        synth[l + "_size"].attrs                ["long_name"] = "size_bin_midpoints_"  + l
     #
     # Three files for individual instruments
     #
@@ -52,6 +57,9 @@ for d in dates:
           "Bin size boundaries measured by "                + acronyms[l] + " (" + l + ")"
         inst[l + "_number_concentration"].attrs["description"] = \
           "Size-resolved number concentration measured by " + acronyms[l] + " (" + l + ")"
+        inst[l + "_size"                ].attrs["long_name"] = "bin_midpoints_"        + l
+        inst[l + "_size_bnds"           ].attrs["long_name"] = "bin_boundaries_"       + l
+        inst[l + "_number_concentration"].attrs["long_name"] = "number_concentration_" + l
     combo = xr.merge([synth, inst])
     combo.attrs = {"creation_date":time.strftime("%Y-%m-%d %H:%M:%S UTC", time.gmtime()),
                    "Conventions":"CF-1.7",
